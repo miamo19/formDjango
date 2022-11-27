601a8697e80c4952b71b18cb8da45b0e3ad408a4
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-11-27 04:16:44 +0100

    Update models.py

--- viewform/scr/Blog/models.py
+++ viewform/scr/Blog/models.py
@@ -8,6 +8,8 @@ class Article(models.Model):
     title   = models.CharField(max_length=100)
     content = models.TextField()
     active  = models.BooleanField(default=False)
+    price   = models.DecimalField(max_digits =15, decimal_places=2, default=99.99 )
+
 
     def get_absolute_url(self):
         return reverse("Blog:article-detail", kwargs={"id":self.id})
